5070fd29b2a7cfd078b57b9ff5d099f84149e287
dimitris-filippatos <dimivanfilip@gmail.com>
2022-07-28 13:46:11 +0300

    add more festivals

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,8 @@ import React from 'react'
 function Home() {
   return (
     <div>
-      Home
+   <hi>What is Partanimal exactly?</hi>
+   <h2>Partanimal is a website that gives you information about the best psytrance festivals around the world. With us you have the change to get familiar with all genres of trance music and share your experience with other people.</h2> 
     </div>
   )
 }
